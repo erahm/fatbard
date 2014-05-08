e2f70068c14aa7c53bebd9b750cf6be4f5a037ba
rockerest <admin@rockerest.com>
2014-05-07 22:50:02 -0500

    Point to web as public

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'mongoid'
 require 'json'
 
+set :public_folder, "web"
+
 get '/' do
-	'Render things here'
+	send_file File.expand_path('index.html', settings.public_folder)
 end
