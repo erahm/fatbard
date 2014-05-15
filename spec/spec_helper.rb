e36e99145973a6a97488d41c418d23e1eaa9d0cb
rockerest <websites@thomasrandolph.info>
2014-05-15 04:48:58 -0500

    Test Authentication route
    I'm pretty sick of writing Ruby now.
    
    i can haz js?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require_relative '../main.rb'
 require_relative '../routes/api/Users.rb'
+require_relative '../routes/api/Authentication.rb'
 require_relative '../controllers/UserController.rb'
 
 require 'rspec'
