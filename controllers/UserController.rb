41077f20017896fe85e33cd32a02f0a6615840fc
Erich Rahm <erahm84@gmail.com>
2014-05-21 22:09:48 -0600

    added update to UserController

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -1,49 +1,87 @@
 require_relative '../models/User.rb'
+require 'active_support'
 require 'pbkdf2'
 require 'openssl'
+require 'mongoid'
 
 class UserController
+    Mongoid.load!('./mongoid.yml')
     attr_accessor :user
 
-    def createUser ( params )
+    def create ( params )
         if params.empty?
             raise ArgumentError
         else
             @user = User.new
+            generateSalt
             params.each_pair do |key, value|
                 if value.to_s.empty?
                     raise "Parameter #{key} is empty"
                 else
-                    generateSalt
                     assignValues(key, value)
                 end
             end
-            # for now we'll run this check. later i'll mock mongo in the specs
-            @user.save unless ENV['RACK_ENV'] == 'test'
+            @user.save
         end
-     end
+    end
+
+    def retrieve ( username )
+        @user = User.where(username: username).first
+        return @user
+    end
+
+    def update ( params )
+        if params.empty?
+            raise ArgumentError
+        else
+            retrieve(params[:username])
+            @user.update(
+                username: params[:username], 
+                password: params[:password], 
+                firstName: params[:firstName], 
+                email: params[:email]
+            )
+        end
+    end
+
+    def authenticate( username, password )
+        self.retrieve( username )
+        thisPass = @user != nil ? hashPassword( password ) : nil
+        return false if !thisPass or (thisPass.hex_string != @user.password)
+        return true
+    end
 
      protected
      def assignValues ( key, value )
         case key
             when "username"
-                @user.username = value
+                user = User.where(username: value)
+                if user.empty?
+                    @user.username = value
+                else
+                    raise "Username already in use"
+                end
             when "email"
                 @user.email = value
             when "firstName"
                 @user.firstName = value
             when "password"
-                @user.password = hashPassword(value)
+                @user.password = hashPassword(value).hex_string
         end
      end
 
      protected
      def hashPassword ( password )
-        return PBKDF2.new(:password => password, :salt => @user.salt, :iterations => 10000)
+        return PBKDF2.new do |p|
+            p.password = password
+            p.salt = @user.salt
+            p.iterations = 10000
+            p.hash_function = OpenSSL::Digest::SHA512
+        end
      end
 
      protected
      def generateSalt
-        @user.salt = OpenSSL::Random.random_bytes 128
+        @user.salt = SecureRandom.base64 64
      end
 end
--- spec/controllers/UserController_spec.rb
+++ spec/controllers/UserController_spec.rb
@@ -8,34 +8,56 @@ describe UserController do
 
     before :each do
         @userController = UserController.new
+        DatabaseCleaner[:mongoid].start
     end
 
-    describe '#createUser' do
+    after :each do
+        DatabaseCleaner[:mongoid].clean
+    end
+
+    describe '#create' do
 
         context 'when parameters are empty' do
             it 'should raise error' do
                 params = {}
-                lambda { @userController.createUser(params) }.should raise_exception ArgumentError
+                lambda { @userController.create(params) }.should raise_exception ArgumentError
             end
         end
 
         context 'when a parameter is empty' do
             it 'should raise error' do
                 params = {:email => "email", :username => ""}
-                lambda { @userController.createUser(params) }.should raise_error
+                lambda { @userController.create(params) }.should raise_error
             end
         end
 
         context 'when username parameter is not empty' do
             it 'should assign value to user.username' do
                 params = {:username => 'joebob'}
+                User.stub(:save) {}
 
                 @userController.instance_variable_set(:@user, User.new)
-                @userController.createUser(params)
+                @userController.create(params)
 
                 user = @userController.instance_variable_get(:@user)
                 user.username.should == params['username']
             end
         end
     end
+
+    describe '#retrieve' do
+
+        context 'when username is passed' do
+            it 'should return a user object' do
+                username = 'username'
+                mockUser = User.create(username: username, password: 'password', firstName: 'joebob', email: 'email@email.com')
+                User.stub(:where).with(username).then_return(mockUser)
+
+                user = @userController.retrieve(username)
+
+                user.should == mockUser
+            end
+        end
+    end
+
 end
