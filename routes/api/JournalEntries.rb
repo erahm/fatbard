d1a6cec99e5b67d517b861723562485a252b3e2c
Erich Rahm <erahm84@gmail.com>
2014-06-19 00:20:17 -0600

    Wrote JournalEntryController methods and added routes

---
+++ routes/api/JournalEntries.rb
@@ -0,0 +1,74 @@
+require 'json'
+require_relative '../../controllers/JournalEntryController.rb'
+require_relative '../../controllers/CampaignController.rb'
+
+module Fatbard
+	module Routes
+		module Api
+			class JournalEntries < Sinatra::Application
+				get '/api/journal/:campaignId' do
+					content_type :json
+
+					campaignController = CampaignController.new
+
+					validateParams(params)
+					campaignController.retrieve(params[:campaignId])
+					journal = campaignController.campaign.journal
+
+					if journal == nil
+						haltCode = 404
+						responseData = nil
+					else
+						haltCode = 200
+						responseData = { journal: journal }.to_json
+					end
+
+					halt haltCode, responseData
+
+				end
+
+				get '/api/journalEntry/:entryId' do
+					content_type :json
+
+					journalEntryController = JournalEntryController.new
+
+					validateParams(params)
+					entry = journalEntryController.retrieve(params[:entryId])
+
+					if entry == nil
+						haltCode = 404
+						responseData = nil
+					else
+						haltCode = 200
+						responseData = { journalEntry: entry }.to_json
+					end
+
+					halt haltCode, responseData
+
+				end
+
+				post '/api/journalEntry/:username' do
+					content_type :json
+
+					journalEntryController = JournalEntryController.new
+
+					requestData = request.POST
+					validateParams(requestData)
+
+					journalEntryController.create(requestData)
+
+					if journalEntryController.journalEntry == nil
+						halt 400
+					else
+						halt 201, response.headers[ 'Location' ] = "/api/journalEntry/#{journalEntryController.journalEntry._id}"
+					end
+				end
+
+				protected
+				def validateParams ( params )
+					halt 400 if params.length == 0
+				end
+			end
+		end
+	end
+end
