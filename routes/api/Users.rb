41ef1e35a028f70830737f140b171db8aadf5ebd
Erich Rahm <erahm84@gmail.com>
2014-08-30 22:45:45 -0600

    Removing exceptions from application flow

--- routes/api/Users.rb
+++ routes/api/Users.rb
@@ -12,17 +12,19 @@ module Fatbard
 
                     requestData = request.POST
                     validateParams(requestData)
+                    username = requestData[:username]
 
-                    begin
+                    if userController.retrieve(username) == nil
+
                         userController.create(requestData)
-                    rescue => error
-                        e = error.to_s
-                        halt 409 if e == "Username already in use"
-                        halt 400, e
+                    else
+                        haltCode = 400
                     end
 
                     user = userController.user
-                    halt 201, response.headers[ 'Location' ] = "/api/user/#{user.username}"
+                    haltCode = 201
+                    responseData = "/api/user/#{user.username}" 
+                    halt haltCode, response.headers[ 'Location' ] = responseData
                 end
 
                 get '/api/user/:username' do
